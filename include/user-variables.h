7fc2b227d0299a0e6b8ed85ca9921d69aac84b69
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2022-04-20 15:35:37 +0300

    moisture sensor var update

--- include/user-variables.h
+++ include/user-variables.h
@@ -3,7 +3,7 @@
 #define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
 #define TIME_TO_SLEEP 30ULL
 uint64_t sleepus = 0;
-const int Dry = 3450;
+const int Dry = 2950;
 const int Wet = 1650;
 const int SensorPin = 32;
 int soilMoistureValue = 0;
